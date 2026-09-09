f0003079d3f791feaec173032cb1fd2c756cbda3
Raul Galleguillos <88955456+Milshakings@users.noreply.github.com>
2026-09-09 17:57:46 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 export const dynamic = 'force-dynamic';
+export const dynamicParams = true;
 export const revalidate = 0;
+export const fetchCache = 'force-no-store';
 
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
