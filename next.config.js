565161c229fd71f1ab8b67678efd7c7e34c0be5f
Raul Galleguillos <88955456+Milshakings@users.noreply.github.com>
2026-09-11 13:23:07 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Aumenta el timeout a 300 segundos (por defecto es 60)
-  staticPageGenerationTimeout: 300,
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
