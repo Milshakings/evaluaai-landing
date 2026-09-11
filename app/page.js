eda276b99aec63f2d9b1c7b625e1b57a687b5669
Raul Galleguillos <88955456+Milshakings@users.noreply.github.com>
2026-09-11 13:26:38 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,16 @@
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
+import Problem from '../components/Problem';
+import HowItWorks from '../components/HowItWorks';
+import ProductShowcase from '../components/ProductShowcase';
 import Audiences from '../components/Audiences';
+import ForWhom from '../components/ForWhom';
 import Comparison from '../components/Comparison';
+import SecurityBlock from '../components/SecurityBlock';
+import ProcurementBadge from '../components/ProcurementBadge';
+import Objections from '../components/Objections';
+import ApiSection from '../components/ApiSection';
+import CTA from '../components/CTA';
 import LeadForm from '../components/LeadForm';
 import Footer from '../components/Footer';
 
@@ -12,18 +21,54 @@ export default function Home() {
         backgroundColor: '#0b0f17',
         minHeight: '100vh',
         color: '#ffffff',
+        overflowX: 'hidden',
       }}
     >
+      {/* Navegación */}
       <Navbar />
+
+      {/* 1. Propuesta de valor principal */}
       <Hero />
+
+      {/* 2. El problema que resolvemos */}
+      <Problem />
+
+      {/* 3. Cómo funciona EvalúaAI */}
+      <HowItWorks />
+
+      {/* 4. Producto / demostración visual */}
+      <ProductShowcase />
+
+      {/* 5. A quién está dirigido */}
       <Audiences />
+
+      {/* 6. Perfiles de usuarios */}
+      <ForWhom />
+
+      {/* 7. Comparación con el proceso tradicional */}
       <Comparison />
+
+      {/* 8. Seguridad y confianza */}
+      <SecurityBlock />
+
+      {/* 9. Compras / implementación institucional */}
+      <ProcurementBadge />
+
+      {/* 10. Respuestas a objeciones */}
+      <Objections />
+
+      {/* 11. Posibilidades de integración */}
+      <ApiSection />
+
+      {/* 12. Llamado a la acción */}
+      <CTA />
+
+      {/* 13. Formulario de contacto / demo */}
       <LeadForm />
+
+      {/* 14. Pie de página */}
       <Footer />
     </main>
   );
 }
-
-
-
 
